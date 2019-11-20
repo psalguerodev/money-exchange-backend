6b5e95b56e341a585c6da862edacfa5a8c9a3eb0
Patrick Salguero <patrick.salguero.avalos@gmail.com>
2019-11-20 09:32:36 -0500

    config(swagger): Change transport type

--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -5,6 +5,7 @@ export function swaggerInit(app: INestApplication) {
   const options = new DocumentBuilder()
     .setTitle('Money Exchange API')
     .setDescription('The Money Exchange API description')
+    .setSchemes("http", "https")
     .setBasePath('/v1')
     .setVersion('1.0')
     .addTag('exchange')
